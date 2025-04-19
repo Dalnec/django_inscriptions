c2aab3825ea1a573142cb19b219e015e618c899f
Dalnec <dalnec1405@gmail.com>
2025-04-18 19:06:30 -0500

    sin cors

--- core/settings/local.py
+++ core/settings/local.py
@@ -7,25 +7,27 @@ ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS  = [
-    'https://*api.tsifactur.com',
-    'https://*.tsifactur.com',
-    'https://*api.tsi.pe',
-    'https://*.tsi.pe',
-    'http://localhost:8000',
-    'http://localhost:5160',
-    'http://192.168.0.111:5160',
-]
+CORS_ALLOWED_ORIGINS  = ['*']
+# CORS_ALLOWED_ORIGINS  = [
+#     'https://*api.tsifactur.com',
+#     'https://*.tsifactur.com',
+#     'https://*api.tsi.pe',
+#     'https://*.tsi.pe',
+#     'http://localhost:8000',
+#     'http://localhost:5160',
+#     'http://192.168.0.111:5160',
+# ]
  
-CSRF_TRUSTED_ORIGINS = [
-    'https://*api.tsifactur.com',
-    'https://*.tsifactur.com',
-    'http://localhost:8000',
-    'https://*api.tsi.pe',
-    'https://*.tsi.pe',
-    'http://localhost:5160',
-    'http://192.168.0.111:5160',
-]
+CSRF_TRUSTED_ORIGINS = ['*']
+# CSRF_TRUSTED_ORIGINS = [
+#     'https://*api.tsifactur.com',
+#     'https://*.tsifactur.com',
+#     'http://localhost:8000',
+#     'https://*api.tsi.pe',
+#     'https://*.tsi.pe',
+#     'http://localhost:5160',
+#     'http://192.168.0.111:5160',
+# ]
 
 DATABASES = {
     'default': env.db(),
