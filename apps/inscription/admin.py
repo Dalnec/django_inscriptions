70ddf0b5d1c78392d84a30e8b09398714b524af7
Dalnec <dalnec1405@gmail.com>
2025-04-20 23:57:31 -0400

    import/export PaymentMethod

--- apps/inscription/admin.py
+++ apps/inscription/admin.py
@@ -3,8 +3,14 @@ from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 from .models import *
 
+class PaymentMethodResource(resources.ModelResource):
+    class Meta:
+        model = PaymentMethod
+
+class PaymentMethodAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    resource_class = PaymentMethodResource
 
 admin.site.register(Inscription)
 admin.site.register(InscriptionGroup)
 admin.site.register(Tarifa)
-admin.site.register(PaymentMethod)
+admin.site.register(PaymentMethod, PaymentMethodAdmin)
