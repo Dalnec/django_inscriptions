c3ed4647a0eca3bf077444233bf416154c458a0c
unknown <dalnec1405@gmail.com>
2025-05-21 00:13:36 -0400

    InscriptionFilter

--- apps/inscription/filters.py
+++ apps/inscription/filters.py
@@ -1,14 +1,27 @@
 import django_filters
+from django.db.models import F, Q
 from rest_framework.pagination import PageNumberPagination
 from .models import *
 
 class InscriptionFilter(django_filters.FilterSet):
     activity = django_filters.CharFilter(field_name="group__activity__id", lookup_expr="exact")
+    search = django_filters.CharFilter(method='search_filter')
+    church = django_filters.CharFilter(field_name="person__church__id", lookup_expr="exact")
     
     class Meta:
         model = Inscription
-        fields = [ 'checkinat', 'status', 'amount', 'observations', 
-                'person', 'activity']
+        fields = [ 'checkinat', 'status', 'amount', 'observations', 'person', 
+                  'activity', 'search', 'church']
+    
+    def search_filter(self, queryset, name, value):
+        if value:
+            queryset = queryset.filter(
+                Q(person__doc_num__icontains=value) |
+                Q(person__names__icontains=value) |
+                Q(person__lastnames__icontains=value) |
+                Q(group__vouchergroup__icontains=value) 
+            )
+        return queryset
 
 class InscriptionPagination(PageNumberPagination):
     page_size_query_param = "page_size"
