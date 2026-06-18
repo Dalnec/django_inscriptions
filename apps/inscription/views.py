8d2d5cffc64d529d07008346541a246ab84170c3
dalnec <dalnec1405@gmail.com>
2026-06-17 23:14:44 -0500

    prints XD

--- apps/inscription/views.py
+++ apps/inscription/views.py
@@ -148,6 +148,8 @@ class InscriptionView(viewsets.GenericViewSet):
     
     def destroy(self, request, pk=None):
         instance = self.get_object()
+        print(request.user)
+        print(request.user.profile)
         if request.user.profile.descripcion.toUpper() == "Administrador".toUpper():
             instance.delete()
             return Response({"message": "Inscripcion eliminada con exito"}, status=status.HTTP_200_OK)
